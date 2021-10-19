cf1fac1929324996a9bd3f1bfbb7581474bf1aa5
Erik van Dijk <Erik@vandijkict.nl>
2021-10-19 16:04:27 +0200

    Created Data Classes and DTOs. Also inluded automapper and change appsettings file

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using BookStore_API.Contracts;
 using BookStore_API.Data;
+using BookStore_API.Mappings;
 using BookStore_API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -44,6 +46,8 @@ namespace BookStore_API
                     .AllowAnyMethod()
                     .AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(Maps));
 
             services.AddSwaggerGen(c =>
             {
